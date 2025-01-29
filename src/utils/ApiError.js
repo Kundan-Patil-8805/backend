0867bc1d174ea1ee78a78d15c3e30b5cd8f92180
Kundan Patil <kpatil.pale@gmail.com>
2025-01-29 10:07:12 +0530

    remove some error

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,23 +1,23 @@
 class ApiError extends Error {
-    constructor( statusCode,
-        message = "somthingg went wrong",   
-        errors=[],
-        statck = "")
-        {
+   constructor(
+       statusCode,
+       message = "Something went wrong",
+       errors = [],
+       stack = ""
+   ) {
        super(message);
-       this.statusCode=  statusCode
-       this.data = null
-       this.message = message
+       this.statusCode = statusCode;
+       this.data = null;
+       this.message = message;
        this.success = false;
-         this.errors = errors
+       this.errors = errors;
 
-         if (stack){
-            this.stack = stack
-         }else {
-            Error.captureStackTrace(this, this.constructor)
-
-         }
-
-         export {ApiError}
-    }
+       if (stack) {
+           this.stack = stack;
+       } else {
+           Error.captureStackTrace(this, this.constructor);
+       }
+   }
 }
+
+export { ApiError };
